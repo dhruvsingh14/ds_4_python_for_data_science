12da12e175c36b23a72d726412c3cfe916e9d8cf
dhruvsingh14 <42729611+dhruvsingh14@users.noreply.github.com>
2020-02-01 21:26:06 -0500

    Delete csv_reader.py

--- wk4/csv_reader.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-# csv_path = "C:\Users\drewn\Desktop\python_for_data_science-master\wk4\NFLX.csv"
-
-# df = pandas.read_csv(csv_path)
-
-df = pandas.read_csv(r"C:\Users\drewn\Desktop\python_for_data_science-master\wk4\NFLX.csv")
-
-print(df)
