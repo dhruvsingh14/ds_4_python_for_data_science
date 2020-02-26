92e9991528ad3c6e1e48d73ce063fb679a4ec8c7
dhruvsingh14 <42729611+dhruvsingh14@users.noreply.github.com>
2020-02-25 21:54:52 -0500

    Add files via upload

--- wk5/wk5_project.py
+++ wk5/wk5_project.py
@@ -23,40 +23,55 @@ def make_dashboard(x, gdp_change, unemployment, title, file_name):
 links = {'GDP': 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/projects/coursera_project/clean_gdp.csv',\
     'unemployment': 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/projects/coursera_project/clean_unemployment.csv'}
 
-##############################
-# q1: creating GDP dataframe #
-##############################
+###############
+# Question 1: #
+###############
+# Create a dataframe that contains the GDP data and
+# display the first five rows of the dataframe.
+
 # pulling data from online platform
 x = pd.read_csv(links["GDP"])
 print(x.head())
 
-
-############################
-# q2: creating a dataframe #
-############################
+###############
+# Question 2: #
+###############
+# Create a dataframe that contains the unemployment data.
+# Display the first five rows of the dataframe.
 x2 = pd.read_csv(links["unemployment"])
 print(x2.head())
 
+###############
+# Question 3: #
+###############
+# Display a dataframe where unemployment was greater than 8.5%.
+print(x2[x2.unemployment > 8.5])
 
-###############################
-# q3: subsetting by threshold #
-###############################
-print(x2[unemployment > 8.5])
+###############
+# Question 4: #
+###############
+# Use the function make_dashboard to make a dashboard
 
+# Create a dataframe with column date
+x_copy = x
+x = x.date
 
+# Create a dataframe with column change-current
+# rename change-current to changecurrent
+x_copy.rename(columns={'change-current': 'changecurrent'}, inplace=True)
+gdp_change = x_copy.changecurrent
 
+# Create a dataframe with column unemployment
+unemployment = x2.unemployment
 
+# Give your dashboard a string title
+title = "US Economy Present Status"
 
+file_name = "US_Economy.html"
 
-
-
-
-
-
-
-
-
-
+# Fill up the parameters in the following function:
+make_dashboard(x=x, gdp_change=gdp_change, unemployment=unemployment,
+            title=title, file_name=file_name)
 
 
 
